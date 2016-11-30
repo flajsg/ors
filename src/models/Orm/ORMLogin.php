9a6c11a226202d40d17c565b00ff3ca886806504
Gregor Flajs <gregor.flajs@ors.si>
2016-11-30 14:49:38 +0100

    ORMLogin: fixed the model so it no longer uses Auth and User models

--- src/models/Orm/ORMLogin.php
+++ src/models/Orm/ORMLogin.php
@@ -1,11 +1,7 @@
-<?php namespace ORM;
+<?php namespace Ors\Orsapi\Orm;
 
-use Illuminate\Database\Eloquent\Model;
-use Eloquent;
-use User;
-use Common;
-use Auth;
-
+use Illuminate\Database\Eloquent\Model as Eloquent;
+use Illuminate\Support\Facades\Config;
 
 /**
  * Login model form ORM.
@@ -23,7 +19,7 @@ class ORMLogin extends Eloquent {
 	protected $fillable = ['agn', 'userid', 'usernm', 'userem', 'ibeid'];
 
 	public function __construct($attributes = array()) {
-		
+		/*
 		// Sometimes only userid is returned inside $attributes, so we must find the other information from User
 		if (empty($attributes['usernm']) && !empty($attributes['userid'])) {
 			$user = User::find($attributes['userid']);
@@ -36,12 +32,12 @@ class ORMLogin extends Eloquent {
 		
 		// if userid is missing than simply use Auth::user->id
 		if (empty($attributes['userid']))
-			$attributes['userid'] = Auth::user()->id;
+			$attributes['userid'] = Auth::user()->id;*/
 		
 		parent::__construct($attributes);
 	}
 
-	public static function withAuthUser() {
+	/*public static function withAuthUser() {
 		$attributes = array(
 			'userid' => Auth::user()->id,
 			'usernm' => Auth::user()->name,
@@ -50,7 +46,7 @@ class ORMLogin extends Eloquent {
 			'ibeid' => Auth::user()->subaccounts->first()->id,
 		);
 		return new self($attributes);
-	}
+	}*/
 	
 	/**
 	 * ibeid accessor.
@@ -58,28 +54,28 @@ class ORMLogin extends Eloquent {
 	 * If user 
 	 * @return int
 	 */
-	public function getIbeidAttribute() {
+	/*public function getIbeidAttribute() {
 		if (!empty($this->attributes['ibeid'])) return $this->attributes['ibeid'];
 		
 		if ($this->user) {
 			return $this->user->subaccounts->first()->id;
 		}
-	}
+	}*/
 	
 	/**
 	 * one-to-one relation (User)
 	 * @return HasOne|Relation|User
 	 */
 	public function user() {
-		return $this->hasOne ( 'User', 'id', 'userid' );
+		return $this->hasOne ( Config::get('orsapi::auth_model'), 'id', 'userid' );
 	}
 	
 	/**
 	 * one-to-one relation (Subaccount)
 	 * @return HasOne|Relation|Subaccount
 	 */
-	public function subaccount() {
+	/*public function subaccount() {
 		return $this->hasOne ( 'Subaccount', 'id', 'ibeid' );
-	}
+	}*/
 	
 }
