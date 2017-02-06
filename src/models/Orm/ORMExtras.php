3e5f8ff28cd28fa2d81c7d12770ee37d6b5aca08
Gregor Flajs <gregor.flajs@ors.si>
2017-02-06 12:43:24 +0100

    added ORMExtras model

---
+++ src/models/Orm/ORMExtras.php
@@ -0,0 +1,11 @@
+<?php namespace Ors\Orsapi\Orm;
+
+use Ors\Orsapi\Oam\OAMAvailabilityExtras;
+
+/**
+ * ORM Extras model.
+ *
+ * Basically the same as OAMAvailabilityExtras but it can have some changes specific for ORM mask.
+ */
+
+class ORMExtras extends OAMAvailabilityExtras {}
